903949176f565349b261784d9c72b7396cfcc747
bigoldcat123 <a1456870251@163.com>
2024-11-25 09:24:44 +0800

    e

--- src/app/(management)/layout.tsx
+++ src/app/(management)/layout.tsx
@@ -1,6 +1,6 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
-import AppBreadCrumb from "@/components/app-breadcrumb";
+import AppBreadCrumb from "@/components/app-breadcrumb"
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 
 export default function Layout({ children }: {
     children: React.ReactNode
@@ -8,12 +8,14 @@ export default function Layout({ children }: {
     return (
         <>
             <SidebarProvider>
-                <AppSidebar />
-                <main>
-                    <div className=" flex items-center">
-                        <SidebarTrigger />
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        {/* <Separator orientation="vertical" className="mr-2 h-4" /> */}
                         <AppBreadCrumb />
                     </div>
+                </header>
+                <main className=" flex-1">
                     {children}
                 </main>
             </SidebarProvider>
